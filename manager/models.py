28ff68563c9a105db383da0e0f165d71cf6cb929
Masih Eskandar <45305878+Gnomy17@users.noreply.github.com>
2021-11-13 16:32:34 +0330

    commit on main

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Mission(models.Model):
+    location = models.CharField(max_length=1000)
+    start_time = models.TimeField(auto_now=False, auto_now_add=False)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False)
+    description = models.CharField(max_length=2000)
