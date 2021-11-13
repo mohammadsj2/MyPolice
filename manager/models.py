384405ba5d5d1af3f7d701561018dadd92710d03
Masih Eskandar <45305878+Gnomy17@users.noreply.github.com>
2021-11-13 17:01:41 +0330

    add model str and add admin stuff

--- manager/models.py
+++ manager/models.py
@@ -6,3 +6,6 @@ class Mission(models.Model):
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
     description = models.CharField(max_length=2000)
+
+    def __str__(self) -> str:
+        return "|loc: " + self.location + "|start: " + str(self.start_time) + "|end: " + str(self.end_time) + "|desc: " + self.description + "|"
