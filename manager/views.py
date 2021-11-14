65a67aa5da00a35a902b2750b195823861bdf335
mohammadsj2 <mohammad.sj2@gmail.com>
2021-11-15 00:18:27 +0330

    Add login, logout, and home page for admin in views.py #24

--- manager/views.py
+++ manager/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from manager.forms import LoginForm
+
+MANAGER_USERNAME = 'manager-username'
+
 
 def index(request):
-    return redirect('login')
+    return redirect('/manager/login/')
+
+
+def home(request):
+    if is_manager_logged_in(request):
+        return render(request, 'manager/home.html', {})
+    else:
+        return redirect('/manager/')
+
+
+def signout(request):
+    if is_manager_logged_in(request):
+        del request.session[MANAGER_USERNAME]
+    return redirect('/manager/')
+
+
+def manager_authentication(username, password) -> bool:
+    return username == 'admin' and password == 'admin'
+
+
+def is_manager_logged_in(request):
+    if request.session.get(MANAGER_USERNAME, None) is None:
+        return False
+    return True
 
 
 def login_page(request):
-    context = {}
-    return render(request, 'manager/login.html', context)
+    if request.method == 'POST':
+        print(request.POST)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            if manager_authentication(username, form.cleaned_data['password']):
+                request.session[MANAGER_USERNAME] = username
+        else:
+            form = LoginForm()
+    else:
+        form = LoginForm()
+
+    if is_manager_logged_in(request):
+        return redirect('/manager/home/')
+    else:
+        return render(request, 'manager/login.html', {'form': form})
