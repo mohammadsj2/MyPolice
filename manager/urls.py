6e5da10339f5a002fcef7a918bf69cb331ca43d8
Keivan <k1rezaei78@gmail.com>
2021-11-15 11:32:27 +0330

    fix urls.py #24

--- manager/urls.py
+++ manager/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_page, name='login'),
     path('home/', views.home, name='home'),
-    path('signout/', views.signout, name='home'),
+    path('signout/', views.sign_out, name='home'),
 ]
