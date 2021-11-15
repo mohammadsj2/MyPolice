76f780b37801dc2e15c3a1cd74a1f63d0b550d7d
mohammadsj2 <mohammad.sj2@gmail.com>
2021-11-15 13:24:55 +0330

    Add CreatePoliceForm #2

--- manager/forms.py
+++ manager/forms.py
@@ -7,8 +7,8 @@ class LoginForm(forms.Form):
 
 
 class CreatePoliceForm(forms.Form):
-    username = forms.CharField(max_length=200, required=True)
-    password = forms.CharField(max_length=200, required=True)
+    username = forms.CharField(label='Username', max_length=200, required=True)
+    password = forms.CharField(widget=forms.PasswordInput(),max_length=200, required=True)
     name = forms.CharField(max_length=200, required=True)
-    gender = forms.ChoiceField(choices=('Man', 'Female', 'Other'), required=True)
+    gender = forms.ChoiceField(choices=(('1', 'Male'), ('2', 'Female')))
     birthday = forms.DateField()
